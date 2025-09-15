d287172dc6938e152227228e6cc60988929d02cf
Nour-El-Houda08 <nourelhouda.nassef@gmail.com>
2025-09-15 14:19:18 +0100

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace NewProject.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
