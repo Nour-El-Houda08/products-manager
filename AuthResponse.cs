dcf1d4246ae78e4d0cb06f5001011cdc4c459c5c
Nour-El-Houda08 <nourelhouda.nassef@gmail.com>
2025-09-15 14:18:13 +0100

    Create AuthResponse.cs

---
+++ AuthResponse.cs
@@ -0,0 +1,9 @@
+namespace NewProject.Models
+{
+    public class AuthResponse
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
